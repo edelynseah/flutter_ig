d8b3fdf59f2aed792de9d5c1fd25e55370031776
edelynseah <edelyn.seah@gmail.com>
2023-02-14 17:39:27 +0800

    upload files

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:proj1/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget WebScreenLayout;
+  final Widget MobileScreenLayout;
+  const ResponsiveLayout({
+    Key? key,
+    required this.WebScreenLayout,
+    required this.MobileScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          //webscreen (in utils)
+          return WebScreenLayout;
+        }
+        return MobileScreenLayout;
+      },
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [SvgPicture.asset(assetName)],
+              ))),
+    );
+  }
+}
